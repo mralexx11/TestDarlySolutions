e343de4d8404dc3ee103857cc7e7fc2d43d3b8ff
Alex <42402960+mralexx11@users.noreply.github.com>
2019-11-14 18:30:15 +0200

    test task auth on angular with firebase

---
+++ src/environments/environment.ts
@@ -0,0 +1,18 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyB4XIvrEMkJ2w5hocTJI5nPL1Mun8_kUU4",
+    authDomain: "test-23c70.firebaseapp.com",
+    databaseURL: "https://test-23c70.firebaseio.com",
+    projectId: "test-23c70",
+    storageBucket: "test-23c70.appspot.com",
+    messagingSenderId: "384646566338",
+    appId: "1:384646566338:web:add436a57aa8e4632f065b",
+    measurementId: "G-THEX0F97C9"
+  }
+};
